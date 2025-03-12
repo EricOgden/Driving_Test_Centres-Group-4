7fd5f2ea59338e953e8032d217dd5ff9e4c3e970
emilyshah1 <159148123+emilyshah1@users.noreply.github.com>
2025-03-12 21:34:32 +0000

    Update pass_rates07-24.py

--- pass_rates07-24.py
+++ pass_rates07-24.py
@@ -2,17 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 
+# Read the file containing years, the number of driving tests conducted each year, and the pass rates for each year
 df = pd.read_csv("passrates07-24.csv")
 
-x = df.iloc[:, 1]
-hist = df.iloc[:, 2]
-y = df.iloc[:, 3]
+x = df.iloc[:, 1] # years
+hist = df.iloc[:, 2] # driving tests conducted
+y = df.iloc[:, 3] # pass rates
 
 print(df.head)
 
 fig, ax1 = plt.subplots(figsize=(10, 5))
 
-ax1.bar(x, hist, width=0.8, alpha=1, color='steelblue', label="Histogram Data")
+ax1.bar(x, hist, width=0.8, alpha=1, color='steelblue', label="Driving Tests Conducted")
 ax1.set_xlabel("Time (years)")
 ax1.set_ylabel("Driving Tests Conducted", color='steelblue')
 ax1.tick_params(axis='y', labelcolor='steelblue')
