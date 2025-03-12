1dc2408aaa81d4c8857bfd4f4feae6dee7d5f48e
emilyshah1 <159148123+emilyshah1@users.noreply.github.com>
2025-03-12 21:58:35 +0000

    Update pass_rates07-24.py

--- pass_rates07-24.py
+++ pass_rates07-24.py
@@ -13,22 +13,28 @@ print(df.head)
 
 fig, ax1 = plt.subplots(figsize=(10, 5))
 
+# Bar chart for driving tests conducted
 ax1.bar(x, hist, width=0.8, alpha=1, color='steelblue', label="Driving Tests Conducted")
 ax1.set_xlabel("Time (years)")
 ax1.set_ylabel("Driving Tests Conducted", color='steelblue')
 ax1.tick_params(axis='y', labelcolor='steelblue')
 
+# Ensures large numbers are displayed for visual appeal
 ax1.yaxis.set_major_formatter(mticker.FuncFormatter(lambda x, _: f'{int(x):,}'))
 ax1.set_yticks(range(0, 2000001, 250000))
 
+# Line graph for pass rate
 ax2 = ax1.twinx()
 ax2.plot(x, y, marker='o', linestyle='-', color='r', label="Pass Rate (%)")
 ax2.set_ylabel("Pass Rate (%)", color='r')
 ax2.tick_params(axis='y', labelcolor='r')
 
+# Set the y-axis range
 ax2.set_ylim(0, 60)
 
+# Set the title
 plt.title("Driving Tests Conducted and Pass Rates")
 ax1.grid(True, which='both', linestyle='--', linewidth=0.5)
 
+# Display
 plt.show()
